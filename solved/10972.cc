7b99563f58c877b020d2e6f1e2c903571e8e155e
TaeAhnK <tenny0124@naver.com>
2025-12-02 20:01:01 +0900

    10972

---
+++ solved/10972.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void answer(int N, vector<int> S)
+{
+    bool flag = true;
+    for (int i = 0; i < N - 1; i++)
+    {
+        if (S[i] <= S[i + 1])
+        {
+            flag = false;
+        }
+    }
+
+    if (flag)
+    {
+        printf("-1\n");
+        return;
+    }
+
+    next_permutation(S.begin(), S.end());
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", S[i]);
+    }
+    printf("\n");
+
+
+}
+
+int main()
+{
+    int N;
+    vector<int> S;
+
+    scanf("%d\n", &N);
+
+    S = vector<int>(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d ", &S[i]);
+    }
+
+    answer(N, S);
+
+    return 0;
+}
