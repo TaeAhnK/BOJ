1a59b8322f47e389d4e74d5cad9d28a495401e00
TaeAhnK <tenny0124@naver.com>
2025-12-05 12:38:32 +0900

    11722

---
+++ solved/11722.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void answer(int N, vector<int> A)
+{
+    int maxV = 0;
+    vector<int> cache(N, 0);
+
+    cache[0] = 1;
+
+    for (int i = 1; i < N; i++)
+    {
+        cache[i] = 1;
+        for (int j = i - 1; 0 <= j; j--)
+        {
+            if (A[j] > A[i])
+            {
+                cache[i] = max(cache[i], cache[j] + 1);
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        maxV = max(maxV, cache[i]);
+    }
+    
+    printf("%d\n", maxV);
+}
+
+int main()
+{
+    int N;
+    vector<int> A;
+
+    scanf("%d\n", &N);
+
+    A = vector<int>(N);
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d ", &A[i]);
+    }
+
+    answer(N, A);
+
+    return 0;
+}
